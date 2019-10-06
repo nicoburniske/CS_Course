6e0ccb69652afaeababae49087569f933273bea9
Trevor Burniske <trevorburniske@gmail.com>
2019-10-06 10:24:29 -0400

    added fastest car method

--- HW1_Race/Main.java
+++ HW1_Race/Main.java
@@ -1,6 +1,11 @@
 //access point for testing
 public class Main {
     public static void main(String args[]){
-
+        Car c1 = new Car("tractorMakers", "tractor9000",2, 4, 1782);
+        Car c2 = new Car("yeep", "elyeep",4, 5, 1836);
+        Car c3 = new Car("transformer", "bumblebee",9000, 1, 2020);
+        Car c4 = new Car("theAliens", "UFO",80, 20, 1999);
+        Race r1 = new Race(c1, c2, c3, c4, "miami", "grandMiami");
+        System.out.println(r1.getWinner().model);
     }
 }
--- HW1_Race/Race.java
+++ HW1_Race/Race.java
@@ -1,11 +1,11 @@
-
-/** This object represents a race event between multiple cars
- *  Requirements: 
- *      - Look and understand the fields included in the class below.
- *      - Create a constructor 
- *      - Create a method getWinner that will return the winning car (the fastest car will win the race)
- *      - Create a method getPodium that returns the top three cars in the race (1st, 2nd, 3rd)
- *      - Create a method addCar that will add a car to the race.
+/**
+ * This object represents a race event between multiple cars
+ * Requirements:
+ * - Look and understand the fields included in the class below.
+ * - Create a constructor
+ * - Create a method getWinner that will return the winning car (the fastest car will win the race)
+ * - Create a method getPodium that returns the top three cars in the race (1st, 2nd, 3rd)
+ * - Create a method addCar that will add a car to the race.
  */
 public class Race {
     //a list of all the cars participating in the race
@@ -16,13 +16,31 @@ public class Race {
     String name;
 
     //constructor
-    public Race(){
-
+    //signature: Car Car Car Car String String -> Race
+    public Race(Car c1, Car c2, Car c3, Car c4, String location, String name) {
+        this.cars = new Car[4];
+        cars[0] = c1;
+        cars[1] = c2;
+        cars[2] = c3;
+        cars[3] = c4;
+        this.location = location;
+        this.name = name;
+        System.out.println(cars[0].model);
     }
 
-    //getWinner 
-    //Purpose statement:
-    //Signature:
+    //getWinner
+    //Purpose statement: says which car won -- person who finished first
+    //Signature: () -> Car
+    public Car getWinner() {
+        Car fastestSoFar = this.cars[0];
+        for (int i = 1; i < cars.length; i++) {
+            if (this.cars[i].topSpeed > fastestSoFar.topSpeed) {
+                fastestSoFar = this.cars[i];
+            }
+        }
+        return fastestSoFar;
+    }
+
 
     //getPodium 
     //Purpose statement:
